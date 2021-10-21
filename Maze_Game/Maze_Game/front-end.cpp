1f38cbb6ad50c81104638694fdc53a0906104dd4
TNOpanska19 <TNOpanska19@codingburgas.bg>
2021-10-21 20:48:27 +0300

    Create temporary (test) menu

--- Maze_Game/Maze_Game/front-end.cpp
+++ Maze_Game/Maze_Game/front-end.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include "front-end.h"
 #include "back-end.h"
+
+void printMenu()
+{
+    cout << "==============================" << endl;
+    cout << "             MENU             " << endl;
+    cout << "==============================" << endl;
+    cout << "                              " << endl;
+    cout << "  Play                   " << endl;
+    cout << "  About        " << endl;
+    cout << "  Team    " << endl;
+    cout << "  Exit          " << endl;
+    cout << "                              " << endl;
+    cout << "==============================" << endl;
+
+    cout << endl << "Select >> ";
+}
